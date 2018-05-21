fa5ef05d95bdd8bd72e903cdb9b75e72c15890c1
Leah Comment <leah.comment@gmail.com>
2018-05-21 15:16:15 -0400

    Initialize to more interesting DGP parameters

--- updateShiny.R
+++ updateShiny.R
@@ -1,6 +1,7 @@
 library("rsconnect")
 rsconnect::deployApp(appDir = "~/Dropbox/Semicompeting-PS/SemiShiny/", 
                      appFiles = c("app.R", "Strategy1models.md",
+                                  "Compartment_shiny.png",
                                   "DESCRIPTION", "README.md"),
                      appName = "Semicompeting-PS",
                      account = "lcomm")
